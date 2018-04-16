fce051c9c0974c82d380804706e47efc51b38563
Turiec <turiec.online@gmail.com>
2018-04-16 11:31:04 +0200

    install script and firefox settings added

---
+++ firefox/user.js
@@ -0,0 +1,4 @@
+# Mozilla User Preferences
+user_pref("browser.startup.page", 3);
+user_pref("browser.download.useDownloadDir", false);
+
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo =====> checking Vim instalation
+command -v vim >/dev/null 2>&1 || {
+    echo >&2 "====> Vim is not installed. Installing...";
+        sudo apt-get install vim; }
+
+echo =====> applying Firefox settings
+cd ~/.mozilla/firefox/
+if [[ $(grep '\[Profile[^0]\]' profiles.ini) ]]
+then PROFPATH=$(grep -E '^\[Profile|^Path|^Default' profiles.ini | grep -1 '^Default=1' | grep '^Path' | cut -c6-)
+else PROFPATH=$(grep 'Path=' profiles.ini | sed 's/^Path=//')
+fi
+cd -
+
+echo ./firefox/user.js ~/.mozilla/firefox/$PROFPATH
+cp ./firefox/user.js ~/.mozilla/firefox/$PROFPAT
