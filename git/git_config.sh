91a796607c66d3242958da3b8d834d17e79e7d5c
turiec <turiec.online@gmail.com>
2019-03-29 13:22:49 +0100

    Update git_config.sh

--- git/git_config.sh
+++ git/git_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "=====> configuring git"
-git config --global alias.hist "log --oneline --graph --decorate --color"
-if [ -f .gitconfig ]; then
-        cp .gitconfig ~/.gitconfig
+#git config --global alias.hist "log --oneline --graph --decorate --color"
+if [ -f git/.gitconfig ]; then
+        cp git/.gitconfig ~/.gitconfig
 fi
